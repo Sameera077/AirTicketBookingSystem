731d4dc72443ddd96e8fcdb09b933dd02d16790d
Sameera077 <sameerashaik1022@gmail>
2025-02-07 15:36:18 +0530

    Completed create booking

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -24,7 +24,23 @@ class BookingRepository {
         }
     }
 
-    async update(data) {
+    async update(bookingId, data) { {
+        try {
+            const booking = await Booking.findByPk(bookingId);
+            if(data.status) {
+                booking.status = data.status;
+            }
+            await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError',
+                'Can not update Booking',
+                'There was some issue updating the booking. Please try again later',
+                StatusCodes.INTERNAL_SERVER_ERROR
+            ) ;
+        }
+    }
         
     }
 }
